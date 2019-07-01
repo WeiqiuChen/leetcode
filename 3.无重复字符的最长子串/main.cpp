31be2daffdea3e17a05d0068f3cd0fb28322a283
WeiqiuChen <550481591@qq.com>
2019-07-01 22:53:06 +0800

    Create main.cpp

---
+++ 3.无重复字符的最长子串/main.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string>
+#include <iostream>
+#include <algorithm>
+#include <map>
+
+using namespace std;
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int maxlen = 0, left = 0, n = s.size();
+		std::map<char,int> m;
+		for(int i=0;i<n;++i){
+			left = max(left,m[s[i]]);
+			m[s[i]] = i+1;
+			maxlen = max(maxlen,i-left+1);
+		}
+		return maxlen;
+		
+        
+    }
+};
+
+
+int main(int argc, char **argv){
+	string s1 = "pwwkew";
+	Solution s;
+	std::cout<<s.lengthOfLongestSubstring(s1)<<endl;
+	getchar();
+	return 0;
+}
