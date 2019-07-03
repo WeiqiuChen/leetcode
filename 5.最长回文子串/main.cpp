deb320e1032319b0476a147c81da58d96bd36b11
WeiqiuChen <550481591@qq.com>
2019-07-03 22:48:11 +0800

    Update main.cpp

--- 5.最长回文子串/main.cpp
+++ 5.最长回文子串/main.cpp
@@ -12,12 +12,18 @@ using namespace std;
  * 长度为2，若s[j]==s[k],则为回文子串,此时长度为2,如"bb"为回文子串，"ba"则不是
  * 长度>2,若s[j]==s[k],且[j+1,k-1]是回文子串,那么[j,k]是回文子串。dp[j][k] = dp[j+1][k-1]+2;
  * 如"a"是回文子串，"bab"前后为相同，那么"bab"为回文子串
+ * 2.中心扩展
+ * babad
+ * iter从0到n-1寻找各个位置最大的回文子串，分为奇偶两种情况
+ * 此时为奇数个，假设迭代到t，此时要判断t-r，t+r是否越边界，且是否s[t-r]==s[t+r]
+ * 此时为偶数个，首先保证s[t]=s[t+1],此时要判断t-r，t+r+1是否越边界，且是否s[t-r]==s[t+r+1]
+ * 3.马拉车方法(之后更新）
  */
 
 class Solution {
 public:
     string longestPalindrome(string s) {
-		int n = s.size();
+		int n=s.size();
         vector<vector <int> > dp(n,vector<int>(n,0));
 		int maxlen=0,idx=0;
 		for(int i=0; i<n; i++){
@@ -34,6 +40,54 @@ public:
 		}
 		return s.substr(idx,maxlen);
     }
+	string longestPalindrome1(string s) {
+		int n=s.size();
+		int pos=0,maxlen=0;
+		for(int i=0;i<n;i++){
+			int r=1;
+			while(i-r>=0&&i+r<n&&s[i-r]==s[i+r]) r++;
+			if(maxlen<2*r-1){
+				maxlen=2*r-1;
+				pos=i-r+1;
+			}
+			if(i>=n-1||s[i]!=s[i+1])continue;
+			r=1;
+			while(i-r>=0&&i+1+r<n&&s[i-r]==s[i+1+r])r++;
+			if(maxlen<2*r){
+				maxlen=2*r;
+				pos=i-r+1;
+			}
+		}
+		return s.substr(pos,maxlen);
+    }
+	string longestPalindrome2(string s) {
+		int n=s.size();
+		int m=2*n+1;
+		vector<char> str(n,'*');
+		vector<int> slen(n,0);
+		for(int i=0;i<n;i++) str[2*i+1]=s[i];
+		int curpos=0,curlen=0,maxpos=0,maxlen=0;
+		for(int i=0;i<m;i++){
+			int mi =2*curpos-i,r=slen[mi];
+			if(i+r<curpos+curlen){
+				slen[i]=slen[mi];
+				continue;
+			}
+			r=curpos+curlen-i;
+			while(i-r>=0&&i+r<n&&str[i-r])==str[i+r])r++;
+			r--;
+			slen[i]=r;
+			curpos=i;
+			curlen=r;
+			if(maxr<r){
+				maxpos=i;
+				maxr=r;
+			}
+			if(i+maxr>=n)break;
+		}
+		return s.substr(maxpos/2-maxr/2,maxr);
+    }
+	
 };
 
 
@@ -42,6 +96,7 @@ int main(int argc, char **argv){
 	
 	Solution s;
 	std::cout<<s.longestPalindrome("babad")<<endl;
+	std::cout<<s.longestPalindrome1("babad")<<endl;
 	getchar();
 	return 0;
 }
