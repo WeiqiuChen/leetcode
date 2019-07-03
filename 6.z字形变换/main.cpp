59788ec94ed47f0d0e1f90815a3335b7672bf306
WeiqiuChen <550481591@qq.com>
2019-07-03 23:18:04 +0800

    Create main.cpp

---
+++ 6.z字形变换/main.cpp
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+/*
+ * https://leetcode-cn.com/problems/zigzag-conversion/
+ */
+
+
+class Solution {
+public:
+    string convert(string s, int numRows) {
+		string str="";
+        int n = s.size();
+		if(numRows==1||n<numRows)return s;
+		int p=0;
+		for(int i=0;i<numRows;i++){
+			p=i;
+			while(p<n){
+				str+=s[p];
+				if(i!=0&&i!=numRows-1&&p+numRows*2-2-2*i<n)
+					str+=s[p+numRows*2-2-2*i];
+				p+=numRows*2-2;
+			}
+		}
+		return str;
+    }
+};
+
+int main(int argc, char **argv){
+	
+	
+	Solution s;
+	std::cout<<s.convert("LEETCODEISHIRING",3)<<endl;
+	std::cout<<s.convert("LEETCODEISHIRING",4)<<endl;
+	getchar();
+	return 0;
+}
